b88bfabfd4d8ff2b803207fffb0224bc2adbe9fa
Kuprienko Anatolii <ipz222_kaa@student.ztu.edu.ua>
2022-11-16 17:35:04 +0200

    Message on first 3 lines changed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("text 1");
+Console.WriteLine("text 2");
+Console.WriteLine("text 3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
