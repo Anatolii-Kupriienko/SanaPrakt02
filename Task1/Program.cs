82793dde97360541fc5eb93a63dfd2beea5d0c31
Kuprienko Anatolii <ipz222_kaa@student.ztu.edu.ua>
2022-11-16 17:45:12 +0200

    commit on branch second

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("text 1");
-Console.WriteLine("text 2");
-Console.WriteLine("text 3");
-Console.WriteLine("text 4");
-Console.WriteLine("text 5");
+Console.WriteLine("Hello World!");
